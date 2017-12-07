58dc81691798d71d45c4fd410b82a86be0b4b5dd
milugloomy <milugloomy@163.com>
2017-12-07 15:49:11 +0800

    post

--- winewx/src/main/resources/public/js/wineShare.js
+++ winewx/src/main/resources/public/js/wineShare.js
@@ -1,7 +1,7 @@
 var Wine={};
 Wine.wineShare=function(shareData){
 	var debug=shareData.debug||false;
-	$.post("wxSign",{url:shareData.link},function(data){
+	post2SRV("wxSign",{url:shareData.link},function(data){
 		wx.config({
 			debug : debug, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
 		 	appId: data.appId, // 必填，公众号的唯一标识
